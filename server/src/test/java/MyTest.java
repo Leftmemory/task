162cdc63bba454a6b781b255c17d619ce5d05545
hzzhangxiaodan <hzzhangxiaodan@corp.netease.com>
2016-02-01 17:54:24 +0800

    增加mongodb配置

--- server/src/test/java/MyTest.java
+++ server/src/test/java/MyTest.java
@@ -1,12 +1,21 @@
+import com.mongodb.DB;
 import com.zxd.task.cache.JedisClient;
+import com.zxd.task.model.Region;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Order;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by zxd on 15/6/15.
@@ -18,6 +27,8 @@ public class MyTest {
 
     @Autowired
     private JedisClient jedisClient;
+    @Autowired
+    private MongoTemplate mongoTemplate;
     private static int flag = 0;
 
     @Test
@@ -27,22 +38,71 @@ public class MyTest {
         System.out.println(obj.toString());
     }
 
+    @Test
+    public void _test() {
+        Set<String> colls = this.mongoTemplate.getCollectionNames();
+        for (String coll : colls) {
+            log.info("--------------CollectionName=" + coll);
+        }
+        DB db = this.mongoTemplate.getDb();
+        log.info("--------------db=" + db.toString());
+    }
 
     @Test
-    public void testRedis() {
-        while (flag < 1000) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    flag++;
-                    while (true) {
-                        jedisClient.setHash("test_zxd","test"+flag,flag,0);
-                        List<String> list = jedisClient.getListRange("mylist");
-                        System.out.println(list.toString());
-                    }
-                }
-            }).start();
+    public void createCollection() {
+        if (!this.mongoTemplate.collectionExists(Region.class)) {
+            this.mongoTemplate.createCollection(Region.class);
         }
-        System.out.println(1);
     }
+
+    @Test
+    public void findListByAge() {
+        int id = 1;
+        Query query = new Query();
+        query.addCriteria(new Criteria("id").is(id));
+        List<Region> list = this.mongoTemplate.find(query, Region.class);
+        for(Region e : list){
+            System.out.println("--------------" + e.toString());
+        }
+    }
+
+    @Test
+    public void findOne() {
+        Query query = new Query();
+        int id = 1;
+        query.addCriteria(new Criteria("id").is(id));
+        Region region = this.mongoTemplate.findOne(query, Region.class);
+        System.out.println("--------------" + region.toString());
+    }
+
+    @Test
+    public void  findOneByUsername() {
+        String regionName = "hz";
+        Query query = new Query();
+        query.addCriteria(new Criteria("regionName").is(regionName));
+        Region region =  this.mongoTemplate.findOne(query, Region.class);
+        System.out.println("--------------" + region.toString());
+    }
+
+    @Test
+    public void insert() {
+        Region entity = new Region();
+        entity.setId(1);
+        entity.setRegionName("hz");
+        this.mongoTemplate.insert(entity);
+    }
+
+    @Test
+    public void update() {
+        Region entity = new Region();
+        entity.setId(1);
+        entity.setRegionName("sjz");
+        Query query = new Query();
+        query.addCriteria(new Criteria("id").is(entity.getId()));
+        Update update = new Update();
+        update.set("id", entity.getId());
+        update.set("regionName", entity.getRegionName());
+        this.mongoTemplate.updateFirst(query, update, Region.class);
+    }
+
 }
